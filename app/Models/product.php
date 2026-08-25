f964d72acb54a8d9c0dcfb8c676e2c04ff018514
Imam Iswanto <imamiswanto4@gmail.com>
2026-08-26 03:22:21 +0700

    feat(stock) : add stock movement relation

--- app/Models/product.php
+++ app/Models/product.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\StockMovement;
 
 class Product extends Model
 {
@@ -44,6 +46,11 @@ class Product extends Model
 
     public function category(): BelongsTo {
     return $this->belongsTo(Category::class);
+    }
+
+    public function stockMovements(): HasMany
+    {
+        return $this->hasMany(StockMovement::class);
     }
 
     protected $fillable = [
